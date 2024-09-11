8c052e6d2f702a7fa382f88e679b75eabf92d685
Brandon Kelly <bkelly8@rockets.utoledo.edu>
2024-09-11 11:56:06 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// Name: Brandon Kelly
+// Class: CSET 3600
+// Instructor: Don Miller
+// Homework: 1 Date: 9/11/2024
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
